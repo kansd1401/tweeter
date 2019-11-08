b586cfb1c5a251aae0acf9197e4c050763535798
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2019-11-08 00:05:17 +0000

    added timestamp functionality on tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,23 +6,24 @@
 
 const renderTweets = function(tweets) {
 // loops through tweets
-  for(let user in tweets){
+  for (let user = tweets.length-1;user => 0;user--) {
     // calls createTweetElement for each tweet
-    const $tweet = createTweetElement(tweets[user])
+    const $tweet = createTweetElement(tweets[user]);
     // takes return value and appends it to the tweets container
     $('.new-tweet').append($tweet);
   }
-}
+};
 
 const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
 const createTweetElement = (tweetData) => {
   const $tweet = $("<article>").addClass("tweet");
-  const safeHtml = escape(tweetData.content.text)
+  const safeHtml = escape(tweetData.content.text);
+  const timeStamp = moment(tweetData.created_at).fromNow();
   const markup = `
         <header>
           <img class="avatar" src="${tweetData.user.avatars}">
@@ -31,65 +32,81 @@ const createTweetElement = (tweetData) => {
         </header>
         <div class="content">${safeHtml}</div>
         <footer> 
-          <div class="life">1${tweetData.created_at}</div>
+          <div class="life">${timeStamp}</div>
           <div class="shortcuts">
           <i class="fas fa-flag"></i>
           <i class="fas fa-retweet"></i>
           <i class="fas fa-heart"></i>
           </div></footer>
         `;
-  $($tweet).prepend(markup);
-  return $tweet
-}
+  $($tweet).append(markup);
+  return $tweet;
+};
 
 const loadTweets = () => {
   $.ajax({url: '/tweets'})
-      .then((res) => {
-        renderTweets(res)
-      })
-      .fail(() => {
-        console.log('dont work')
-      });
-}
+    .then((res) => {
+      renderTweets(res);
+    })
+    .fail(() => {
+      console.log('dont work');
+    });
+};
 
-const warn = () => {$(".warning").slideToggle()}
+const warn = () => {
+  $(".warning").slideToggle();
+};
 
 //When pages loaded starts working on the script
 $(() => {
-  $("#new-post").animate({'margin-top': "-160px"});
+  $("#new-post").animate({'margin-top': "-240px"});
   loadTweets();
-  warn()
+  warn();
   let hidden = true;
-  $('#new-post').submit(function(event){
+  $('#new-post').submit(function(event) {
     event.preventDefault();
-    const data = $(this).serialize()
+    const data = $(this).serialize();
     const $content = $("#input").val();
-    if($content.length > 140){
-      warn()
-      setTimeout(warn,3000)
-    }else if($content.length === 0){
-      warn()
-      setTimeout(warn,3000)
-    }else{
+    if ($content.length > 140) {
+      warn();
+      setTimeout(warn,3000);
+    } else if ($content.length === 0) {
+      warn();
+      setTimeout(warn,3000);
+    } else {
       $.ajax({
         type: 'POST',
         url: '/tweets',
-      data: data})
-      .then(()=> {
-        $("#input").val('')
-        $('.new-tweet').empty()
-        loadTweets()
-      })
+        data: data})
+        .then(()=> {
+          $("#input").val('');
+          $('.new-tweet').empty();
+          loadTweets();
+        });
     }
   });
   $('#show-post').click(() => {
-    if(hidden){
+    if (hidden) {
       $("#new-post").animate({'margin-top': "00px"});
-      $("#input").focus()
+      $("#input").focus();
       hidden = false;
-    }else{
-      $("#new-post").animate({'margin-top': "-160px"});
+    } else {
+      $("#new-post").animate({'margin-top': "-240px"});
+      $("#input").val('');
       hidden = true;
     }
-  })
+  });
 });
+$(document)
+  .one('focus.autoExpand', 'textarea.autoExpand', function() {
+    let savedValue = this.value;
+    this.value = '';
+    this.baseScrollHeight = this.scrollHeight;
+    this.value = savedValue;
+  })
+  .on('input.autoExpand', 'textarea.autoExpand', function() {
+    let minRows = this.getAttribute('data-min-rows') | 0, rows;
+    this.rows = minRows;
+    rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 16);
+    this.rows = minRows + rows;
+  });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(() => {
-  $('textarea').keyup(function(){
-    let char = 140-$(this).val().length
-    $('#counter').text(char)
-    if(char < 0){
-      $('#counter').css('color','#ff0000')
-    }else{
-      $('#counter').css('color','#000000')
+  $('textarea').keyup(function() {
+    let char = 140 - $(this).val().length;
+    $('#counter').text(char);
+    if (char < 0) {
+      $('#counter').css('color','#ff0000');
+    } else {
+      $('#counter').css('color','#000000');
     }
   });
 });
